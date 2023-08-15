44cc6d9d0cf61a9d500655ef1e8a24cd9dafc11e
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-15 17:26:48 +0100

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,8 @@
--- List the columns and their data types of the table first_table in the specified database
-SELECT COLUMN_NAME, DATA_TYPE
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = '$1' AND TABLE_NAME = 'first_table';
+-- Print the full description of the table first_table
+SELECT 'Table   Create Table';
+SELECT CONCAT(TABLE_NAME, ' ', CREATE_TABLE_STMT)
+FROM (
+    SELECT TABLE_NAME, CREATE_TABLE_STMT
+    FROM information_schema.TABLES
+    WHERE TABLE_SCHEMA = '$1' AND TABLE_NAME = 'first_table'
+) AS t;
