1587513a74b4221e3c5218a5de139432684172c9
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-15 15:07:20 +0100

    list databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
-/*This script lists all databases on a MySQL server.
+/*This script lists all databases on a MySQL server
 Prompt the user for MySQL password*/
 mysql -u root -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" > updated_databases.txt
