68c7bef80877f5f52e9b70a50777cc4b34014133
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-15 15:31:20 +0100

    list databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,4 @@
-/*List all databases on a MySQL server
-Connect to the MySQL server*/
-mysql -u root -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;"
---List all databases
+-- List all databases on a MySQL server
+
+-- List all databases
 SHOW DATABASES;
