00d610a9843ea5144b8756ae866d4540d55db2ca
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-15 17:14:15 +0100

    LIST ALL IN THE TABLE

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,4 @@
--- Describe the table first_table in the specified database
-USE $1;
-DESCRIBE first_table;
+-- List the columns and their data types of the table first_table in the specified database
+SELECT COLUMN_NAME, DATA_TYPE
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = '$1' AND TABLE_NAME = 'first_table';
