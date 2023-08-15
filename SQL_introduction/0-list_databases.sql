79f5bb9c4d83fedf20e73c535d8e903f2f42dc76
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-15 14:59:48 +0100

    list databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,12 @@
-/*List all databases on a MySQL server
-Connect to the MySQL server*/
-mysql -u root -p
---List all databases
-SHOW DATABASES;
+/*This script lists all databases on a MySQL server.
+Prompt the user for MySQL password*/
+read -s -p "Enter MySQL password: " MYSQL_PASSWORD
+echo
+
+/*Connect to the MySQL server and execute the query
+List all databases*/
+mysql -u root -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" > initial_databases.txt
+
+/*Create additional databases if needed
+Run the query again and capture the updated databases*/
+mysql -u root -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;" > updated_databases.txt
