e6b6225df1d6134043ba10ee9a53b379eb53ece9
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-15 18:09:59 +0100

    creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
 
 -- Insert multiple rows into second_table
 INSERT INTO hbtn_0c_0.second_table (id, name, score)
-VALUES (1, 'Chioma', 16),
-       (2, 'Alex', 12),
-       (3, 'John', 7),
-       (4, 'Fatima', 20);
+VALUES (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
