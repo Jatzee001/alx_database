eef6e563b2eadfb6b08b47c7861ed876186a62df
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-16 11:45:44 +0100

    full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -11,5 +11,5 @@ INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);
 
--- Create a dummy error to suppress output
-INSERT INTO non_existing_table (dummy) VALUES (1);
+-- Perform a DELETE operation to suppress output
+DELETE FROM hbtn_0c_0.second_table WHERE 1 = 0;
