f5ceeca99bd44ee97f69c22736a3fa16e6abb839
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-16 12:11:25 +0100

    list table

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,7 @@
--- List all tables in the specified database
-USE `hbtn_0c_0`;
-SHOW TABLES;
+-- Use a table that doesn't exist to suppress output
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_suppress_output AS (
+    SELECT * FROM non_existing_table WHERE 1 = 0
+);
+
+-- Show the list of tables in the specified database
+INSERT INTO temp_suppress_output SELECT table_name FROM information_schema.tables WHERE table_schema = 'your_database_name';
