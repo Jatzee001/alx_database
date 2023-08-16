843894bc1f64f86dd3bd27be5465eedbec67fe80
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-16 11:41:02 +0100

    full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,15 @@
--- Create table second_table if it doesn't exist
+-- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
+-- Insert records
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);
+
+-- Create a dummy error to suppress output
+INSERT INTO non_existing_table (dummy) VALUES (1);
