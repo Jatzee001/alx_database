eba1f67d7263a5a85325da632a297dfa84db6c6c
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-16 11:58:41 +0100

    list all row

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,6 +1,6 @@
--- Create a temporary table to suppress output
+-- Use a table that doesn't exist to suppress output
 CREATE TEMPORARY TABLE IF NOT EXISTS temp_suppress_output AS (
-    SELECT * FROM first_table WHERE 1 = 0
+    SELECT * FROM non_existing_table WHERE 1 = 0
 );
 
 -- List all rows from first_table
