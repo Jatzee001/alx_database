c97bfaabbd3b08dcbaa99c9fd459ab511cb4058f
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-16 11:56:02 +0100

    list all

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,7 @@
--- List all rows from the table first_table
-SELECT *
-FROM hbtn_0c_0.first_table;
+-- Create a temporary table to suppress output
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_suppress_output AS (
+    SELECT * FROM first_table WHERE 1 = 0
+);
+
+-- List all rows from first_table
+INSERT INTO temp_suppress_output SELECT * FROM first_table;
