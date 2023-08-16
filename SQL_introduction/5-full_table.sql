47ba7cf238be1b4b0b392545c9a8ced460360620
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-16 12:04:06 +0100

    full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,7 @@
--- List the columns and their data types of the table first_table in the specified database
-SELECT COLUMN_NAME, DATA_TYPE
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = '$1' AND TABLE_NAME = 'first_table';
+-- Use a table that doesn't exist to suppress output
+CREATE TEMPORARY TABLE IF NOT EXISTS temp_suppress_output AS (
+    SELECT * FROM non_existing_table WHERE 1 = 0
+);
+
+-- Show the CREATE TABLE statement for first_table
+INSERT INTO temp_suppress_output SELECT * FROM information_schema.tables WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
