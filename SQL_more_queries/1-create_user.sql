264b13911a6693f7874eb7b2f536fc114f7c917c
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-17 13:00:55 +0100

    root

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
 -- Set the MySQL root password
 MYSQL_ROOT_PASSWORD="08105659056Mtn."
+USER_0D_1_PASSWORD="qwertyuiop"
 
 -- Execute the 1-create_user.sql script
 mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD < 1-create_user.sql
 
 -- Execute the 0-privileges.sql script
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD < 0-privileges.sql
+mysql -hlocalhost -uuser_0d_1 -p$USER_0D_1_PASSWORD < 0-privileges.sql
