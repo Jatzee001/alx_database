e79d99e1a7446884e383dfd6a68b458f2576e8d6
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-17 14:18:50 +0100

    null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,29 +1,10 @@
 -- Set the MySQL root password
-MYSQL_ROOT_PASSWORD="your_root_password"
+MYSQL_ROOT_PASSWORD="08105659056Mtn."
 DATABASE_NAME="$1" # Accept the database name as an argument
 
--- Check if the id_not_null table exists
-EXIST=$(mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e "SHOW TABLES LIKE 'id_not_null';")
-if [ -z "$EXIST" ]; then
-    echo "id_not_null doesn't exist"
-else
-    echo "id_not_null exists"
-fi
-
--- Execute the 4-never_empty.sql_no_comment script to create the table
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME < 4-never_empty.sql_no_comment
-
--- Insert data into the id_not_null table
-echo 'INSERT INTO id_not_null (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
-
--- Insert data without specifying the id (default value used)
-echo 'INSERT INTO id_not_null (name) VALUES ("Holberton");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
-
--- Attempt to insert data without specifying the name (expected error)
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e "INSERT INTO id_not_null (id) VALUES (333);"
-
--- Display the content of the id_not_null table
-echo 'INSERT INTO id_not_null (name) VALUES ("Holberton");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
-
--- Display the content of the id_not_null table again
-echo 'INSERT INTO id_not_null (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
+-- Create the id_not_null table using SQL commands
+echo "USE $DATABASE_NAME;
+CREATE TABLE id_not_null (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256)
+);" | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD
