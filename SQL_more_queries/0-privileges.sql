1be6f778acff793c1c2613db947f7970a23b8c7f
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-17 12:48:23 +0100

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Set the MySQL root password
-MYSQL_ROOT_PASSWORD="your_root_password"
+MYSQL_ROOT_PASSWORD="08105659056Mtn."
 
 -- Execute the 0-privileges.sql script
 mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD < 0-privileges.sql
