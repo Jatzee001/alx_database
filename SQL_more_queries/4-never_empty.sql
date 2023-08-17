986274ee87cfd91bd44d985ca07669a3742e082a
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-17 13:49:10 +0100

    ID can't be null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -2,9 +2,17 @@
 MYSQL_ROOT_PASSWORD="your_root_password"
 DATABASE_NAME="$1" # Accept the database name as an argument
 
--- Execute the SQL commands to create the id_not_null table
-echo "USE $DATABASE_NAME;
-CREATE TABLE id_not_null (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256)
-);" | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD
+-- Execute the 4-never_empty.sql script to create the table
+mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME < 4-never_empty.sql
+
+-- Insert data into the id_not_null table
+echo 'INSERT INTO id_not_null (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
+
+-- Display the content of the id_not_null table
+echo 'SELECT * FROM id_not_null;' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
+
+-- Insert data without specifying the id (default value used)
+echo 'INSERT INTO id_not_null (name) VALUES ("Holberton");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
+
+-- Display the content of the id_not_null table again
+echo 'SELECT * FROM id_not_null;' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
