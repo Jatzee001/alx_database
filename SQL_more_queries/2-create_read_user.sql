eba85f72eb2c61648d18a00483ab25de208adf31
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-17 13:19:12 +0100

    read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,9 @@
 -- Set the MySQL root password
-MYSQL_ROOT_PASSWORD="08105659056Mtn."
-USER_0D_2_PASSWORD="qwertyuiop1234"
+MYSQL_ROOT_PASSWORD="your_root_password"
+USER_0D_2_PASSWORD="user_0d_2_pwd"
+
+-- Check if user user_0d_2 exists
+mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD -e "SELECT user FROM mysql.user WHERE user = 'user_0d_2';" 2>&1 | grep -q 'user_0d_2' && echo "user_0d_2 exists" || echo "user_0d_2 doesn't exist"
 
 -- Create the database hbtn_0d_2 if not exists
 mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS hbtn_0d_2;"
@@ -11,6 +14,9 @@ mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS 'u
 -- Grant SELECT privilege to user_0d_2 on the hbtn_0d_2 database
 mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';"
 mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';"
+
+-- Try to login with user_0d_2
+mysql -hlocalhost -uuser_0d_2 -p$USER_0D_2_PASSWORD -e "SHOW DATABASES;"
 
 -- Execute the 0-privileges.sql script
 cat 0-privileges.sql | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD
