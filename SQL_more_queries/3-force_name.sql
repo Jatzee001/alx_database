2c35506f7c4128d469e77a3ad8e6ca643df7e336
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-17 13:42:40 +0100

    Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,21 +1,18 @@
 -- Set the MySQL root password
-MYSQL_ROOT_PASSWORD="08105659056Mtn."
+MYSQL_ROOT_PASSWORD="your_root_password"
 DATABASE_NAME="hbtn_0d_2"
 
--- Check if the force_name table exists
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e "DESCRIBE force_name;" 2>&1 | grep -q 'Table.*does not exist' && echo "force_name doesn't exist" || echo "force_name exists"
-
 -- Execute the 3-force_name.sql script to create the table
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME < 3-force_name.sql
+cat 3-force_name.sql | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
 
 -- Insert data into the force_name table
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e 'INSERT INTO force_name (id, name) VALUES (89, "Holberton School");'
+echo 'INSERT INTO force_name (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
 
--- Display the content of the force_name table
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e 'SELECT * FROM force_name;'
-
--- Attempt to insert data without specifying the name (expected error)
+-- Insert data without specifying the name (expected error)
 mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e "INSERT INTO force_name (id) VALUES (333);"
 
--- Display the content of the force_name table again
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e 'SELECT * FROM force_name;'
+-- Attempt to insert data with a NULL name (expected error)
+echo 'INSERT INTO force_name (id, name) VALUES (333, NULL);' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
+
+-- Display the content of the force_name table
+cat 3-show_data.sql | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
