b66f81f3f6e5c0e6fd364d9ef732837ac060782c
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-30 18:34:10 +0100

    0. My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,23 +1,5 @@
--- MySQL connection configuration
-MYSQL_CMD="mysql -hlocalhost -uroot -p"
+-- Show privileges for user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Function to list privileges of a user
-list_privileges() {
-    local username="$1"
-    echo "Privileges for $username@localhost:"
-    $MYSQL_CMD -e "SHOW GRANTS FOR '$username'@'localhost';"
-}
-
--- Check if users exist and list their privileges
-echo "Checking users and their privileges:"
-$MYSQL_CMD -e "SELECT user FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');" | grep user_0d_1
-if [ $? -eq 0 ]; then
-    list_privileges user_0d_1
-else
-    echo "Users don't exist"
-
-$MYSQL_CMD -e "SELECT user FROM mysql.user WHERE user = 'user_0d_2';" | grep user_0d_2
-if [ $? -eq 0 ]; then
-    list_privileges user_0d_2
-else
-    echo "Users don't exist"
+-- Show privileges for user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
