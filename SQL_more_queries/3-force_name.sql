d800fcb44c50f9f12e5c08ba28c87f6844a92cfc
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-30 18:40:31 +0100

    3. Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,18 +1,8 @@
--- Set the MySQL root password
-MYSQL_ROOT_PASSWORD="your_root_password"
-DATABASE_NAME="hbtn_0d_2"
+-- Replace 'your_database_name' with the actual database name where you want to create the table
+USE your_database_name;
 
--- Execute the 3-force_name.sql script to create the table
-cat 3-force_name.sql | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
-
--- Insert data into the force_name table
-echo 'INSERT INTO force_name (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
-
--- Insert data without specifying the name (expected error)
-mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME -e "INSERT INTO force_name (id) VALUES (333);"
-
--- Attempt to insert data with a NULL name (expected error)
-echo 'INSERT INTO force_name (id, name) VALUES (333, NULL);' | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
-
--- Display the content of the force_name table
-cat 3-show_data.sql | mysql -hlocalhost -uroot -p$MYSQL_ROOT_PASSWORD $DATABASE_NAME
+-- Create table force_name if not exists
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
